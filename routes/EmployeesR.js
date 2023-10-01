40076b7a2079e17a0e3aa1d58c2a8291ef595136
MeitalHilf <tool16289@gmail.com>
2023-10-01 20:36:37 +0300

    added crud employees

--- routes/EmployeesR.js
+++ routes/EmployeesR.js
@@ -24,9 +24,47 @@ router.post("/Add", function(req,res){
 
 });
 
+router.patch("/Edit", function(req,res){
+    let id =req.body.id;
+    let name =req.body.name;
+    // יצירת שאילתה לשמירת שורה
+    let Query=`UPDATE \`employees\` SET \`name\`='${name}' WHERE id='${id}'`;
+    console.log("update employee",Query);
+    db_pool.query(Query, function(err, rows, fields){
+
+        if(err){
+            res.status(500).json({message: err})
+            // throw err;a
+        }else{
+            res.status(200).json({message: "OK",lastId:rows.insertId});
+        }
+
+    });
+
+});
+
+router.delete("/Delete", function(req,res){
+    let id =req.body.id;
+    // יצירת שאילתה לשמירת שורה
+    let Query=`DELETE FROM \`employees\` WHERE id='${id}'`;
+    console.log("delete employee",Query);
+    db_pool.query(Query, function(err, rows, fields){
+
+        if(err){
+            res.status(500).json({message: err})
+            // throw err;
+        }else{
+            res.status(200).json({message: "OK",lastId:rows.insertId});
+        }
+
+    });
+
+});
+
+
 router.get("/List",(req, res) => {
 
-    let q="SELECT * FROM `categories` ";
+    let q="SELECT * FROM `employees` ";
 
     db_pool.query(q, function(err, rows, fields){
 
