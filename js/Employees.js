e1a587d0f6492ffe7e6a6a1acfc6d0203befb9a1
MeitalHilf <tool16289@gmail.com>
2023-10-02 20:39:32 +0300

    crud employees js

--- js/Employees.js
+++ js/Employees.js
@@ -1,38 +1,60 @@
-let raw_data=[];
-let srchTerm="";
-// filter Data
+let raw_data = [];
 
-}
-// Create
 function CreateTable(){
-    srchTerm=document.getElementById("filterField").value;
-    let data=raw_data.filter(FilterData);
     let str="";
-    for(let line of data){
+    for(let line of raw_data){
         str+="<tr>";
-        str+=`<td> <input type="text" name="namei"  id="namei-'+line.id+'"  placeholder="name"/></td>`;
         str+=`<td><button onclick="editLine(${line.id});">edit</button></td>`;
-        str+="<td>"+line.id+"</td>";
         str+="<td>"+line.name+"</td>";
         str+=`<td><button onclick="deleteLine(${line.id});">delete</button></td>`;
         str+="</tr>";
     }
-    document.getElementById("mainEmployees").innerHTML=str;
+    document.getElementById("oneEmployees").innerHTML=str;
 }
 
-// Read
+
 async function getList() {
-    let response = await fetch('/employees/List');
+        let response = await fetch('/employees/List');
+        let data = await response.json();
+        console.log("data=", data);
+        raw_data = data;
+        CreateTable();
+    }
+
+async function addNewLine() {
+    let name=document.getElementById("name").value;
+    let response = await fetch('/employees/Add', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({name: name})
+        }
+    );
     let data = await response.json();
-    console.log("data=",data);
-    raw_data = data;
-    CreateTable();
+    console.log(data);
+    await getList();
 }
 
-// Update
+async function deleteLine(id) {
+    let objToServer={};
+    objToServer.id=id;
+    let response = await fetch('/employees/Delete', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(objToServer)
+        }
+    );
+// let data = await response.json();
+// console.log(data);
+    await getList();
+}
+
 async function editLine(id) {
     let objToServer={};
-    let name = document.getElementById(`namei-${id}`).value;
+    let name = document.getElementById(`namei-${id}`).value
     objToServer.id=id;
     objToServer.name=name;
     let response = await fetch('/employees/Edit', {
@@ -43,37 +65,8 @@ async function editLine(id) {
             body: JSON.stringify(objToServer)
         }
     );
-    getList();
+
+    await getList();
 }
-getList();
-// Delete
-async function deleteLine(id) {
-    let objToServer={};
-    objToServer.id=id;
-    let response = await fetch('/employees/Del', {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(objToServer)
-        }
-    );
-    // let data = await response.json();
-    // console.log(data);
-    getList();
-}
-// Add
-async function addNewLine() {
-    let name=document.getElementById("name").value;
-    let response = await fetch('/employees/Add', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({name:name})
-        }
-    );
-    let data = await response.json();
-    console.log(data);
-    getList();
-}
+
+getList()
